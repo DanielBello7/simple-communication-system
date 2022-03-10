7abcb0d47929fd4677f6acde6361ac2c22125368
Daniel Bello <gokebello@gmail.com>
2022-03-10 14:15:18 +0100

    creating the base formation
    creating the dashboard system and the login system and creating a state system to handle and modify render upon user login.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
 
 
 
-export default function App(){
+import Login from './components/Login';
+import Dashboard from './components/dashboard/Dashboard';
+
+function App() {
   return (
     <div>App</div>
   )
 }
+export default App;
